2cadd6a7981b846d7c35179358d325bc0c46235e
xmcnulty <x.mcnulty@gmail.com>
2016-08-16 19:14:44 -0400

    Still just a binary tree

--- MyRBTree.c
+++ MyRBTree.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 // initialize a new node.
-Node* initNode(void *_data) {
+Node* initNode(void *data) {
 	Node* n = (Node*) calloc(1, sizeof(Node));
 	
 	if(!n) {
@@ -14,16 +14,105 @@ Node* initNode(void *_data) {
 		exit(-1);
 	}
 	
-	n->color = RED;
-	n->data = _data;
+	n->_color = RED;
+	n->_data = data;
+
+    return n;
 }
 
 // initialize a new tree.
 Tree* initTree() {
-	return (Tree*) calloc(1, sizeof(Tree));
+    Tree *tree = (Tree*) calloc(1, sizeof(Tree));
+
+	return tree;
 }
 
-// TESTING ONLY
-int main() {
-	return 0;
+// insert a node into the tree
+void insert(Tree* tree, void *data, compare cFunc) {
+    Node *node = initNode(data);
+
+    // case 1: tree is empty
+    if(tree->_size == 0) {
+        tree->_root = node;
+        node->_color = BLACK;
+
+        tree->_size ++;
+
+        return;
+    }
+
+    Node *ptr = tree->_root;
+
+    int comparison = cFunc(data, ptr->_data), res;
+
+    if (comparison == 0)
+        return;
+    else if(comparison == -1)
+        res = insertLeft(ptr, node, cFunc);
+    else
+        res = insertRight(ptr, node, cFunc);
+
+    if (res) {
+        //TODO: Balance tree.
+
+        tree->_size ++;
+    }
+
+    return;
+}
+
+// recursive function that inserts to the left of a node
+int insertLeft(Node *ptr, Node *newNode, compare cFunc) {
+    if(ptr->_left == NULL) { // insert to the left of ptr
+        ptr->_left = newNode;
+        newNode->_parent = ptr;
+
+        return 1;
+    }
+
+    ptr = ptr->_left;
+
+    int comparison = cFunc(newNode->_data, ptr->_data);
+
+    if (comparison == 0)
+        return 0;
+    else
+        return (comparison < 0) ? insertLeft(ptr, newNode, cFunc) : insertRight(ptr, newNode, cFunc);
+}
+
+// recursive function that inserts to the right of a node
+int insertRight(Node *ptr, Node *newNode, compare cFunc) {
+    if(ptr->_right == NULL) { // insert to the right of ptr
+        ptr->_right = newNode;
+        newNode->_parent = ptr;
+
+        return 1;
+    }
+
+    ptr = ptr->_right;
+
+    int comparison = cFunc(newNode->_data, ptr->_data);
+
+    if (comparison == 0)
+        return 0;
+    else
+        return (comparison < 0) ? insertLeft(ptr, newNode, cFunc) : insertRight(ptr, newNode, cFunc);
+}
+
+// query if a tree contains a value
+int search(Tree* tree, void *data, compare cFunc) {
+    if (tree->_size == 0)
+        return 0;
+
+    return searchHelper(tree->_root, data, cFunc);
+}
+
+// recursive helper function for search
+int searchHelper(Node *ptr, void *data, compare cFunc) {
+    int comparison = cFunc(ptr->_data, data);
+
+    if (comparison == 0)
+        return 1;
+    else
+        return (comparison < 0) ? searchHelper(ptr->_left, data, cFunc) : searchHelper(ptr->_right, data, cFunc);
 }
--- MyRBTree.h
+++ MyRBTree.h
@@ -6,19 +6,38 @@ typedef struct rb_tree Tree;
 typedef int (*compare)(void*, void*);
 
 struct rb_node {
-	void *data;
-	enum {RED, BLACK} color;
-	struct rb_node *left, *right, *parent;
+	void *_data;
+	enum {RED, BLACK} _color;
+	struct rb_node *_left, *_right, *_parent;
 };
 
 struct rb_tree {
-	Node* root;
-	int size;
+	Node *_root;
+	int _size;
 };
 
 // initialize a new node.
-Node* initNode(void *_data;
+Node* initNode(void *data);
 
 Tree* initTree();
 
-void insert(Tree* t, void *_data, compare *c);
+// insert node into red black tree
+// cFunc is a pointer to a user function that compares the
+// values being stored in the tree with the following signature:
+// int cFunc(void* val1, void* val2)
+// 		if val1 == val2 it returns 0
+//		else if val1 < val2 it returns <0
+//		else if val1 > val2 it returns
+void insert(Tree* tree, void *data, compare cFunc);
+
+// recursive function that inserts to the left of a node
+int insertLeft(Node *ptr, Node *newNode, compare cFunc);
+
+// recursive function that inserts to the right of a node
+int insertRight(Node *ptr, Node *newNode, compare cFunc);
+
+// query if a tree contains a value
+int search(Tree* tree, void *data, compare cFunc);
+
+// recursive helper function for search
+int searchHelper(Node *ptr, void *data, compare cFunc);
